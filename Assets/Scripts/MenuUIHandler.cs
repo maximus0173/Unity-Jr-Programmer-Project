4e444068e3c7ec4f46357bcfafde35c3a977634b
maximus0173 <93214280+maximus0173@users.noreply.github.com>
2023-09-14 20:22:04 +0200

    prepared start menu scene

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -7,29 +7,13 @@ using UnityEngine.SceneManagement;
 using UnityEditor;
 #endif
 
-// Sets the script to be executed later than all default scripts
-// This is helpful for UI, since other things may need to be initialized before setting the UI
 [DefaultExecutionOrder(1000)]
 public class MenuUIHandler : MonoBehaviour
 {
-    public ColorPicker ColorPicker;
-
-    private void Start()
-    {
-        ColorPicker.Init();
-        //this will call the NewColorSelected function when the color picker have a color button clicked.
-        ColorPicker.onColorChanged += NewColorSelected;
-        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
-    }
 
     public void StartNew()
     {
         SceneManager.LoadScene(1);
-    }
-
-    public void NewColorSelected(Color color)
-    {
-        MainManager.Instance.TeamColor = color;
     }
 
     public void Exit()
@@ -40,17 +24,6 @@ public class MenuUIHandler : MonoBehaviour
 #else
         Application.Quit();
 #endif
-    }
-
-    public void SaveColorClicked()
-    {
-        MainManager.Instance.SaveColor();
-    }
-
-    public void LoadColorClicked()
-    {
-        MainManager.Instance.LoadColor();
-        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
     }
 
 }
